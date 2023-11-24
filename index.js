8f1c139ce54a0d434a10ada434c3ef9edf8fbe94
Jonathan Lao <jon.lao@gmail.com>
2023-11-23 22:21:59 -0600

    updated files

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 // The code below is temporary and can be commented out.
 const { fetchCoordsByIP } = require('./iss');
+const { fetchISSFlyOverTimes } = require('./iss');
 
 // fetchCoordsByIP('204.83.32.109', (error, coordinates) => {
 //   if (error) {
@@ -11,3 +12,14 @@ const { fetchCoordsByIP } = require('./iss');
 
 //   console.log('It worked! Returned coordinates:' , coordinates);
 // });
+
+
+const exampleCoords = { latitude: '49.27670', longitude: '-123.13000' };
+
+fetchISSFlyOverTimes(exampleCoords, (error, data) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+  console.log('It worked! Returned flyover times:' , data);
+});
